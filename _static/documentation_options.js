6f04094869b0b35bee3b78157e16644f5df4bc9d
tiekoetter <leon@tiekoetter.com>
2024-08-04 14:33:53 +0000

    [doc] build from commit 3b30f75c28b17a719ca5e08e9d6e14ee892ffa28

---
+++ _static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '2024.8.4+3b30f75c',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
